65bd2e350048e34c839519363ca367436ca1f82d
Lecoeuchee <julien.lecoeuche1st@gmail.com>
2021-10-18 12:26:27 +0200

    Using material components

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,67 +1,43 @@
 import 'package:flutter/material.dart';
 
-class MyAppBTar extends StatelessWidget {
-  const MyAppBar({required this.title, Key? key}) : super(key: key);
+void main(List<String> args) {
+  runApp(
+    const MaterialApp(
+      title: 'Flutter Tuto',
+      home: TutorialHome(),
+    ),
+  );
+}
 
-  final Widget title;
+class TutorialHome extends StatelessWidget {
+  const TutorialHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      child: Row(
-        children: [
-          const IconButton(
-            onPressed: null,
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-          ),
-          Expanded(
-            child: title,
-          ),
-          const IconButton(
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+          onPressed: null,
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+        ),
+        title: const Text('Title'),
+        actions: const [
+          IconButton(
             onPressed: null,
             icon: Icon(Icons.search),
             tooltip: 'Search',
           ),
         ],
       ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: [
-          MyAppBar(
-            title: Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.headline6,
-            ),
-          ),
-          const Expanded(
-            child: Center(
-              child: Text("Hello, world!"),
-            ),
-          ),
-        ],
+      body: const Center(
+        child: Text('Hello world'),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Add',
+        child: Icon(Icons.add),
       ),
     );
   }
-}
-
-void main(List<String> args) {
-  runApp(
-    const MaterialApp(
-      title: 'My App',
-      home: TutorialHome(child: MyScaffold()),
-    ),
-  );
 }
